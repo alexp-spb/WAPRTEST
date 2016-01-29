459c7f6defba84a269d956b516676570c44e5d81
Aleks <administrator@TRIXDC.trixdom.local>
2016-01-29 09:18:55 +0300

    v 17

--- T1/Test1/Test1/Comp.cs
+++ T1/Test1/Test1/Comp.cs
@@ -12,7 +12,7 @@ namespace Test1
          //throw new Exception("Feature 10 not working !!!");
           // ver 5
           //
-         Console.WriteLine("Ver 15");
+         Console.WriteLine("Ver 17");
      }
 
     }
